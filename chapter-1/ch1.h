66d0239633216544b1501fbb49dc024a3585a307
Oliems <100418841+Oliems@users.noreply.github.com>
2022-06-14 19:41:32 +0200

    Declared constants IN and OUT and rm_com function

--- chapter-1/ch1.h
+++ chapter-1/ch1.h
@@ -1,10 +1,16 @@
 #ifndef CH1_H_
 #define CH1_H_
 
+#define IN 1
+#define OUT 0
+
 int get_line(char[], int);
 void rm_blank(char[], char[]);
 void copy(char[], char[]);
 void reverse(char[], char[]);
 void fold(char[], char[], int);
+void rm_com(char[], char[]);
+
+int comment;
 
 #endif
