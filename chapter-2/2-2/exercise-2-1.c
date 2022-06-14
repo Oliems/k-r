4966f9ab4e34e39e71ed5d12e74b6bbe567099b2
Oliems <100418841+Oliems@users.noreply.github.com>
2022-06-14 15:56:45 +0200

    Solved exercise-2-1 by printing the values from standard headers

--- chapter-2/2-2/exercise-2-1.c
+++ chapter-2/2-2/exercise-2-1.c
@@ -8,9 +8,34 @@
 
 #include <stdio.h>
 #include <limits.h>
-#include <floats.h>
+#include <float.h>
 
 int main(void) {
+  /* Values of integer types as defined in limits.h */
+  printf("Maximimum and minimum of values of integers types as defined in limits.h.\n\n");
+  printf("Maximum value of char is : %d\n", CHAR_MAX);
+  printf("Minimum value of char is : %d\n", CHAR_MIN);
+  printf("Maximum value of int is : %d\n", INT_MAX);
+  printf("Minimum value of int is : %d\n", INT_MIN);
+  printf("Maximum value of long is : %ld\n", LONG_MAX);
+  printf("Minimum value of long is : %ld\n", LONG_MIN);
+  printf("Maximum value of signed char is : %d\n", SCHAR_MAX);
+  printf("Minimum value of signed char is : %d\n", SCHAR_MIN);
+  printf("Maximum value of short is : %d\n", SHRT_MAX);
+  printf("Minimum value of short is : %d\n", SHRT_MIN);
+  printf("Maximum value of unsigned char is : %d\n", UCHAR_MAX);
+  printf("Maximum value of unsigned int is : %u\n", UINT_MAX);
+  printf("Maximum value of unsigned long is : %lu\n", ULONG_MAX);
+  printf("Maximum value of unsigned short is : %d\n\n", USHRT_MAX);
+
+  /* Values of floating point types as defined in float.h */
+  printf("Maximimum and minimum of values of floating point types as defined in float.h.\n\n");
+  printf("Maximum value of float is : %f\n", FLT_MAX);
+  printf("Minimum value of float is : %f\n", FLT_MIN);
+  printf("Maximum value of double is : %f\n", DBL_MAX);
+  printf("Minimum value of double is : %f\n", DBL_MAX);
+  printf("Maximum value of long double is : %Lf\n", LDBL_MAX);
+  printf("Minimum value of long double is : %Lf\n", LDBL_MAX);
 
   return 0;
 }
