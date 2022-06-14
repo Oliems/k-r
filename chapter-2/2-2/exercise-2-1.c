6789fe0eea43e6b9788b57a61750da3fc064c0c5
Oliems <100418841+Oliems@users.noreply.github.com>
2022-06-14 15:17:53 +0200

    Initial commit for exercise-2-1

---
+++ chapter-2/2-2/exercise-2-1.c
@@ -0,0 +1,16 @@
+/*
+** Write a program to determine the range of char, short, int and long
+** variables, both signed and unsigned, by printing appropriate values
+** from standard headers and by direct computation. Harder if you
+** compute them: determine the range of the various floating-point
+** types.
+*/
+
+#include <stdio.h>
+#include <limits.h>
+#include <floats.h>
+
+int main(void) {
+
+  return 0;
+}
