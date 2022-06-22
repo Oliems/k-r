f97c1542093cadccd440946b8e8c01d72487cd10
Oliems <100418841+Oliems@users.noreply.github.com>
2022-06-22 16:36:53 +0200

    Moved files into a flat directory structure

---
+++ chapter-2/exercise-2-10.c
@@ -0,0 +1,11 @@
+/*
+** Rewrite the function lower, which converts upper case letters to lower case,
+** with a conditional expression instead of if-else.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+  return 0;
+}
