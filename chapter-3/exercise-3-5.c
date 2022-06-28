ff1f7a0bee62010f2dd6f5f8ccb48ed709695883
Oliems <100418841+Oliems@users.noreply.github.com>
2022-06-28 18:40:56 +0200

    Initial commit for chapter 3's exercises

---
+++ chapter-3/exercise-3-5.c
@@ -0,0 +1,10 @@
+/*
+** Write the function itob(n,s,b) that converts the integer n into a base b
+** charatcer representation in the string s. In particular, itob(n,s,16) formats
+** n as an hexadecimal integer in s.
+*/
+
+int main(void) {
+
+  return 0;
+}
---
+++ chapter-3/exercise-3-6.c
@@ -0,0 +1,10 @@
+/*
+** Write a version of itoa that accepts three arguments instead of two. The
+** third argument is a minimum fiel width; the converted integer must be padded
+** with blanks on the left if necessary to make it wide enough.
+*/
+
+int main(void) {
+
+  return 0;
+}
